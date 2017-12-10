7676facfdd24c0b7f511ee69d9494b00db6d1f1c
Kent Hu <khu.1998@icloud.com>
2017-12-10 19:03:07 +1000

    Fixed up some database things

--- dev/db.py
+++ dev/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from flask_login import UserMixin
@@ -8,14 +8,6 @@ engine = create_engine(db_string)
 
 Base = declarative_base()
 
-try:
-    Base.metadata.create_all(engine)
-except:
-    pass
-
-Base.metadata.bind = engine
-Session = sessionmaker(bind = engine)
-
 class User(Base, UserMixin):
     __tablename__ = "user"
 
@@ -23,9 +15,6 @@ class User(Base, UserMixin):
     username = Column(String, nullable = False, unique = True)
     fname = Column(String, nullable = False)
     lname = Column(String, nullable = False)
+    password = Column(String, nullable = False)
 
-class Password(Base):
-    __tablename__ = "password"
-
-    id = Column(Integer, primary_key = True)
-    val = Column(String, nullable = False)
+Base.metadata.create_all(engine)
