0ad20af3cd7f5aa884999eb223ce5b2cb4251909
NAMTRAN\nam tran <tdnam39@gmail.com>
2017-12-13 23:09:09 +1100

    update interface

--- dev/views.py
+++ dev/views.py
@@ -2,7 +2,12 @@ from flask import render_template
 from datetime import datetime
 from dev.server import app
 
+#Home page before login
 @app.route('/')
+def start():
+	return render_template("base.html")
+
+#Home page after login
 @app.route('/home')
 def home():
     """Renders the home page."""
@@ -11,3 +16,13 @@ def home():
         title = "Home",
         year = datetime.now().year
     )
+
+#Videos Category
+@app.route('/category')
+def category():
+	return render_template("category.html")
+
+@app.route('/signup')
+def signup():
+	return render_template("signup.html")
+
